a204fa5647f863866d5ebc7770b5bae9cf09aec3
thomas.willson@me.com <willson556@users.noreply.github.com>
2009-11-21 05:35:36 +0000

    Revert flvstreamer due to compatibility issues. Will figure out the issue when I have time. Quickly releasing 1.1.2.1.

--- Download.m
+++ Download.m
@@ -31,7 +31,7 @@
 	NSString *getiPlayerPath = [bundlePath stringByAppendingString:@"/Contents/Resources/get_iplayer.pl"];
 	NSString *mplayerPath = [bundlePath stringByAppendingString:@"/Contents/Resources/mplayer"];
 #ifdef __i386__
-	NSString *flvstreamerPath = [bundlePath stringByAppendingString:@"/Contents/Resources/flvstreamer-1.9"];
+	NSString *flvstreamerPath = [bundlePath stringByAppendingString:@"/Contents/Resources/flvstreamer_macosx"];
 #else
 	NSString *flvstreamerPath = [bundlePath stringByAppendingString:@"/Contents/Resources/flvstreamer_macosx"];
 #endif
