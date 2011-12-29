eab83dd30ad96d332175fd4a08157395f6d3e7c9
thomas.willson@me.com <willson556@users.noreply.github.com>
2011-12-29 21:52:35 +0000

    No need to log launch path.

--- ITVDownload.m
+++ ITVDownload.m
@@ -319,7 +319,6 @@
     pipe = [[NSPipe alloc] init];
     errorPipe = [[NSPipe alloc] init];
     [task setLaunchPath:[[NSBundle mainBundle] pathForResource:@"rtmpdump-2.4" ofType:nil]];
-    [self addToLog:[NSString stringWithFormat:@"LaunchPath: %@",[task launchPath]] noTag:YES];
     
     /* rtmpdump -r "rtmpe://cp72511.edgefcs.net/ondemand?auth=eaEc.b4aodIcdbraJczd.aKchaza9cbdTc0cyaUc2aoblaLc3dsdkd5d9cBduczdLdn-bo64cN-eS-6ys1GDrlysDp&aifp=v002&slist=production/" -W http://www.itv.com/mediaplayer/ITVMediaPlayer.swf?v=11.20.654 -y "mp4:production/priority/CATCHUP/e48ab1e2/1a73/4620/adea/dda6f21f45ee/1-6178-0002-001_THE-ROYAL-VARIETY-PERFORMANCE-2011_TX141211_ITV1200_16X9.mp4" -o test2 */
     
